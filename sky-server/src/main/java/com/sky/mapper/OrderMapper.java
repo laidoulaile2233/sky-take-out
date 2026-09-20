2113a0ac2dde3521f9d21ada910a7514cdb55fbb
laidoulaile2233 <3259303840@qq.com>
2026-09-20 19:10:35 +0800

    销量排名Top10

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -79,4 +79,11 @@ public interface OrderMapper {
      */
     Integer countByMap(Map map);
 
+    /**
+     * 查询商品销量排名
+     * @param begin
+     * @param end
+     */
+    List<GoodsSalesDTO> getSalesTop10(LocalDateTime begin, LocalDateTime end);
+
 }
