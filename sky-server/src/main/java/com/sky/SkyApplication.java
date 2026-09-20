9855f6362d35bd9529d1de92e99dd5d08bf9207f
laidoulaile2233 <3259303840@qq.com>
2026-09-20 18:42:55 +0800

    客户催单

--- sky-server/src/main/java/com/sky/SkyApplication.java
+++ sky-server/src/main/java/com/sky/SkyApplication.java
@@ -11,7 +11,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement //开启注解方式的事务管理
 @Slf4j
 @EnableCaching
-@EnableScheduling //开启任务调度
+//@EnableScheduling //开启任务调度（暂时关闭：停用 OrderTask、WebSocketTask 的定时任务）
 public class SkyApplication {
     public static void main(String[] args) {
         SpringApplication.run(SkyApplication.class, args);
--- sky-server/src/main/java/com/sky/service/OrderService.java
+++ sky-server/src/main/java/com/sky/service/OrderService.java
@@ -97,4 +97,10 @@ public interface OrderService {
      */
     void complete(Long id);
 
+    /**
+     * 用户催单
+     * @param id
+     */
+    void reminder(Long id);
+
 }
